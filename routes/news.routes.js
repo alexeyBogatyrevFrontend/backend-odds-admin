9aa770c6b37639ef7df004a757a7f3ebb867a6f7
Bogatyrev Alexey <alexey.bogatyrev.frontend@gmail.com>
2023-12-26 18:40:42 +0600

    add some changed for frontend

--- routes/news.routes.js
+++ routes/news.routes.js
@@ -21,7 +21,7 @@ router.post('/add', upload.single('image'), async (req, res) => {
 		const { id, title, description, textEditor, isTop, date } = req.body
 
 		// Access the file data as a Buffer
-		const imageBuffer = req.file.buffer
+		const imageBuffer = req.file ? req.file.buffer : undefined
 
 		const news = new News({
 			id,
@@ -34,7 +34,48 @@ router.post('/add', upload.single('image'), async (req, res) => {
 		})
 
 		const savedNews = await news.save()
-		res.json(savedNews)
+		const allNews = await News.find()
+
+		res.json(allNews)
+	} catch (error) {
+		res.status(500).json({ error: error.message })
+	}
+})
+
+// Update news by ID
+router.put('/edit/:id', upload.single('image'), async (req, res) => {
+	try {
+		const newsId = req.params.id
+		const { id, title, description, textEditor, isTop, date } = req.body
+
+		// Access the file data as a Buffer if an image is provided
+		const imageBuffer = req.file ? req.file.buffer : undefined
+
+		// Find the news by ID
+		const existingNews = await News.findById(newsId)
+
+		if (!existingNews) {
+			return res.status(404).json({ error: 'News not found' })
+		}
+
+		// Update the news fields
+		existingNews.id = id
+		existingNews.title = title
+		existingNews.description = description
+		existingNews.textEditor = textEditor
+		existingNews.isTop = isTop
+		existingNews.date = date
+
+		// Update the image if provided
+		if (imageBuffer) {
+			existingNews.image = imageBuffer
+		}
+
+		// Save the updated news
+		const updatedNews = await existingNews.save()
+		const allNews = await News.find()
+
+		res.json(allNews)
 	} catch (error) {
 		res.status(500).json({ error: error.message })
 	}
@@ -53,8 +94,9 @@ router.delete('/delete/:id', async (req, res) => {
 
 		// Delete the news
 		await News.findByIdAndDelete(newsId)
+		const allNews = await News.find()
 
-		res.json({ message: 'News deleted successfully' })
+		res.json(allNews)
 	} catch (error) {
 		res.status(500).json({ error: error.message })
 	}
