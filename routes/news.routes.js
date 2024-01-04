ff082a16dfc12fa7b271b6274fd631c2010a6134
Bogatyrev Alexey <alexey.bogatyrev.frontend@gmail.com>
2024-01-04 15:40:50 +0600

    fix edit top news

--- routes/news.routes.js
+++ routes/news.routes.js
@@ -88,49 +88,49 @@ router.post('/add', upload.single('image'), async (req, res) => {
 	}
 })
 
-router.put('/edit/:id', upload.single('image'), async (req, res) => {
-	try {
-		const newsId = req.params.id
-		const { title, description, textEditor, isTop, date } = req.body
+// router.put('/edit/:id', upload.single('image'), async (req, res) => {
+// 	try {
+// 		const newsId = req.params.id
+// 		const { title, description, textEditor, isTop, date } = req.body
 
-		const imageBuffer = req.file ? req.file.buffer : undefined
+// 		const imageBuffer = req.file ? req.file.buffer : undefined
 
-		const existingNews = await News.findById(newsId)
+// 		const existingNews = await News.findById(newsId)
 
-		if (!existingNews) {
-			return res.status(404).json({ error: 'News not found' })
-		}
+// 		if (!existingNews) {
+// 			return res.status(404).json({ error: 'News not found' })
+// 		}
 
-		existingNews.title = title
-		existingNews.description = description
-		existingNews.textEditor = textEditor
-		existingNews.isTop = isTop
-		existingNews.date = date
+// 		existingNews.title = title
+// 		existingNews.description = description
+// 		existingNews.textEditor = textEditor
+// 		existingNews.isTop = isTop
+// 		existingNews.date = date
 
-		if (imageBuffer) {
-			existingNews.image = imageBuffer
-		}
+// 		if (imageBuffer) {
+// 			existingNews.image = imageBuffer
+// 		}
 
-		const updatedNews = await existingNews.save()
-		// const allNews = await News.find()
+// 		const updatedNews = await existingNews.save()
+// 		// const allNews = await News.find()
 
-		// res.json(allNews.reverse())
-		// Используйте переданные значения currentPage и pageSize
-		const currentPage = parseInt(req.query.currentPage) || 1
-		const pageSize = parseInt(req.query.pageSize) || 6
+// 		// res.json(allNews.reverse())
+// 		// Используйте переданные значения currentPage и pageSize
+// 		const currentPage = parseInt(req.query.currentPage) || 1
+// 		const pageSize = parseInt(req.query.pageSize) || 6
 
-		// Получите новости для текущей страницы
-		const skip = (currentPage - 1) * pageSize
-		const allNews = await News.find().skip(skip).limit(pageSize)
+// 		// Получите новости для текущей страницы
+// 		const skip = (currentPage - 1) * pageSize
+// 		const allNews = await News.find().skip(skip).limit(pageSize)
 
-		res.json({
-			newsList: allNews.reverse(),
-			currentPage: currentPage,
-		})
-	} catch (error) {
-		res.status(500).json({ error: error.message })
-	}
-})
+// 		res.json({
+// 			newsList: allNews.reverse(),
+// 			currentPage: currentPage,
+// 		})
+// 	} catch (error) {
+// 		res.status(500).json({ error: error.message })
+// 	}
+// })
 
 // router.delete('/delete/:id', async (req, res) => {
 // 	try {
@@ -162,6 +162,58 @@ router.put('/edit/:id', upload.single('image'), async (req, res) => {
 // 		res.status(500).json({ error: error.message })
 // 	}
 // })
+router.put('/edit/:id', upload.single('image'), async (req, res) => {
+	try {
+		const newsId = req.params.id
+		const { title, description, textEditor, isTop, date } = req.body
+
+		const imageBuffer = req.file ? req.file.buffer : undefined
+
+		const existingNews = await News.findById(newsId)
+
+		if (!existingNews) {
+			return res.status(404).json({ error: 'News not found' })
+		}
+
+		existingNews.title = title
+		existingNews.description = description
+		existingNews.textEditor = textEditor
+		existingNews.isTop = isTop
+		existingNews.date = date
+
+		if (imageBuffer) {
+			existingNews.image = imageBuffer
+		}
+
+		const updatedNews = await existingNews.save()
+
+		const currentPage = parseInt(req.query.currentPage) || 1
+		const pageSize = parseInt(req.query.pageSize) || 6
+		const isTopNews = req.query.topNews === 'true'
+
+		// Пересчитайте количество новостей
+		const totalCount = isTopNews
+			? await News.countDocuments({ isTop: true })
+			: await News.countDocuments()
+
+		// Вычислите новый сдвиг
+		const skip = (currentPage - 1) * pageSize
+
+		// Получите новости для текущей страницы
+		const query = isTopNews ? { isTop: true } : {}
+		const allNews = await News.find(query).skip(skip).limit(pageSize)
+
+		res.json({
+			newsList: allNews.reverse(),
+			totalPages: Math.ceil(totalCount / pageSize),
+			currentPage,
+			isTopNews,
+		})
+	} catch (error) {
+		res.status(500).json({ error: error.message })
+	}
+})
+
 router.delete('/delete/:id', async (req, res) => {
 	try {
 		const newsId = req.params.id
@@ -187,7 +239,7 @@ router.delete('/delete/:id', async (req, res) => {
 		// const newCurrentPage = Math.min(currentPage, totalPages)
 
 		// Вычислите новый сдвиг
-		const skip = (currentPage - 1) * pageSize
+		const skip = (newCurrentPage - 1) * pageSize
 
 		// Получите новости для текущей страницы
 		const query = isTopNews ? { isTop: true } : {}
